55681f4255ccdc4bf638bac145ef71cb3e9dda0a
jayeshsawani1505 <162999881+jayeshsawani1505@users.noreply.github.com>
2024-10-12 17:22:50 +0530

    Add files via upload

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName></engineName>
+		<engineLink></engineLink>
+		<homePageLink>https://electroplatingplants.com</homePageLink>
+		<apis>
+			<api name="ajtechsolution" blogID="1" preferred="true" apiLink="https://electroplatingplants.com/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://electroplatingplants.com/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://electroplatingplants.com/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://electroplatingplants.com/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://electroplatingplants.com/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
